0702d1353cd8c7339208b5c620082e9317222098
Shruti Shrestha <shruti.shresthaa@gmail.com>
2024-11-03 13:15:49 -0500

    build: add setup.py for packaging and update requirements.txt with additional dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="weather_api",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    description="A package for retrieving weather data",
+)
